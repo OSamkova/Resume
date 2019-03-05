89408e7d13739ffe22f632d3b1b68b24a9872de5
Oksana Samkova <oksasamkova@gmail.com>
2019-03-05 15:37:26 -0500

    wp

--- src/App.js
+++ src/App.js
@@ -4,17 +4,43 @@ class App extends Component {
     render() {
         return (
             <div className='resume-container'>
-                <h1 className='resume-header'>
-                    Oksana Samkova-Plattner
-                </h1>
                 <div className='resume'>
                     <div className='left-col'>
+                        <h1 className='resume-header'>
+                            Oksana Samkova-Plattner
+                        </h1>
+                        <div className='contacts-container'>
+                            <div className='contacts-row'>
+                                <div className='icon-wrap'>
+                                    <span className='icon icon-home-solid'/>
+                                </div>
+                                <span className='contacts-cell-item'>Queens, NY</span>
+                            </div>
+                            <div className='contacts-row'>
+                                <div className='icon-wrap'>
+                                    <span className='icon icon-phone-solid'/>
+                                </div>
+                                <span className='contacts-cell-item'>(347) 517-5844</span>
+                            </div>
+                            <div className='contacts-row'>
+                                <div className='icon-wrap'>
+                                    <span className='icon icon-envelope-solid'/>
+                                </div>
+                                <span className='contacts-cell-item'>oksasamkova@gmail.com</span>
+                            </div>
+                            <div className='contacts-row'>
+                                <div className='icon-wrap'>
+                                    <span className='icon icon-github-brands'/>
+                                </div>
+                                <span className='contacts-cell-item'>https://github.com/osamkova</span>
+                            </div>
+                        </div>
                         <div className='about-container'>
                             <h4>About</h4>
                             <p>
-                                I’m a front-end web engineer who loves to solve challenging problems in original, inventive ways. 
-                                I’m self-taught and highly driven, with a flair for aesthetics. 
-                                My skills have continuously grown over the last six years, and now I’m looking for a team that will help me reach the next level.
+                                Front-end engineer with a passion for problem solving. 
+                                Inventive, curious and self-taught I find the javascript ecosystem to be the perfect playground. 
+                                Looking for a team to push my skills into the next level.
                             </p>
                         </div>
 
@@ -26,13 +52,14 @@ class App extends Component {
                                     <span className='date-place'>Jul 2017 - Present</span>
                                 </div>
                                 <div className='experience-title'>
-                                    <span  className='position'>Software Engineer</span>
+                                    <span >Software Engineer</span>
                                     <span className='date-place'>New York, NY</span>
                                 </div>
                                 <div className='description'>
-                                    Developed and built an underwriting application from the ground up, which processed on average 2 billion in loans per year.
-                                    Implemented unit tests using Jest and Enzyme and greatly improved robustness and reliability.
-                                    <p className='cursive'>Technologies used: React/Flux, Jest/Enzyme</p>
+                                    Collaborated on the front-end of our Underwriting AI technology that helped process over 2 billions in loans. 
+                                    My focus was on optimizing the user experience, developing custom features/components and state management and 
+                                    implementing unit tests using Jest and Enzyme.
+                                    <p className='cursive'>Technologies used: React/Redux, Jest/Enzyme</p>
                                 </div>
                             </div>
                             <div className='experience-item'>
@@ -62,8 +89,8 @@ class App extends Component {
                                 </div>
                                 <div className='description'>
                                     Served as a lead quality assurance engineer for an in-house team developing due diligence app for a major commercial mortgage lender. 
-                                    Defined and implemented an automated test suite from the scratch. 
-                                    Supervised and mentored junior team members.
+                                    I defined and implemented an automated test suite from the scratch which greatly improved robustness and reliability of the app. 
+                                    I also supervised and mentored junior team members.
 
                                     <p className='cursive'>Technologies used: GhostInspector, Selenium, NightwatchJS/Browserstack</p>
                                 </div>
@@ -72,34 +99,8 @@ class App extends Component {
                         
                     </div>
                     <div className='right-col'>
-                        <div className='contacts-container'>
-                            <h4>Contact</h4>
-                            <div className='contacts-table'>
-                                <div className='contacts-row'>
-                                    <div className='icon-wrap'>
-                                        <span className='icon icon-home-solid'/>
-                                    </div>
-                                    <span className='contacts-cell-item'>Queens, NY</span>
-                                </div>
-                                <div className='contacts-row'>
-                                    <div className='icon-wrap'>
-                                        <span className='icon icon-phone-solid'/>
-                                    </div>
-                                    <span className='contacts-cell-item'>(347) 517-5844</span>
-                                </div>
-                                <div className='contacts-row'>
-                                    <div className='icon-wrap'>
-                                        <span className='icon icon-envelope-solid'/>
-                                    </div>
-                                    <span className='contacts-cell-item'>oksasamkova@gmail.com</span>
-                                </div>
-                                <div className='contacts-row'>
-                                    <div className='icon-wrap'>
-                                        <span className='icon icon-github-brands'/>
-                                    </div>
-                                    <span className='contacts-cell-item'>https://github.com/osamkova</span>
-                                </div>
-                            </div>
+                        <div className='photo-wrap'>
+                            <img src='https://s3.us-east-2.amazonaws.com/oksanas-misc-photos/IMG_4183.jpg' height="100" width="100"/>
                         </div>
                         <div className='skills-container'>
                             <h4>Skills</h4>
